4024879116cbea6e9c307deee2903626348ad35c
AbdulRahman Hassn <bodahassan12@gmail.com>
2026-08-12 18:31:51 +0300

    Apply a new color combination, and a dark theme

--- javascript.js
+++ javascript.js
@@ -1,2 +1,34 @@
-// TODO: Apply the dark theme
+// DONE: Apply the dark theme
 // TODO: Trigger the hamburger menu
+
+const toggleBtn = document.getElementById("theme-toggle");
+const htmlEl = document.documentElement;
+
+const savedTheme = localStorage.getItem("theme") || "light";
+htmlEl.setAttribute("data-theme", savedTheme);
+updateButton(savedTheme);
+
+toggleBtn.addEventListener("click", () => {
+  const currentTheme = htmlEl.getAttribute("data-theme");
+  const newTheme = currentTheme === "light" ? "dark" : "light";
+
+  htmlEl.setAttribute("data-theme", newTheme);
+  localStorage.setItem("theme", newTheme);
+  updateButton(newTheme);
+});
+
+function updateButton(theme) {
+  const themeIcon = toggleBtn.querySelector("i");
+  const isDark = theme === "dark";
+
+  if (themeIcon) {
+    themeIcon.className = isDark
+      ? "fa-solid fa-sun fa-fw"
+      : "fa-solid fa-moon fa-fw";
+  }
+
+  toggleBtn.setAttribute(
+    "aria-label",
+    isDark ? "Switch to light theme" : "Switch to dark theme",
+  );
+}
